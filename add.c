a89609e41bfde952d3650461605360a59f58c69f
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-13 03:05:08 -0700

    fixed add.c file

--- add.c
+++ add.c
@@ -7,13 +7,13 @@
 */
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	int sum = (*stack)->n + (*stack)->n;
+	int sum = (*stack)->n + (*stack)->next->n;
 
 	pop(stack, line_number);
 	(*stack)->n = sum;
