7cdde7e3ed6b6fe651324e7424a38edb7e4eb182
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-13 01:12:43 -0700

    working on task files

--- monty.h
+++ monty.h
@@ -1,8 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+
+extern stack_t *stack;
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -33,4 +36,11 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void push(stack_t **stack, int value);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 #endif
