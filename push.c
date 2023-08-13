09617e359d6c03aa49f216467b62fa9caf7a79b1
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-13 02:53:49 -0700

    needed to fix spelling errors and betty in push.c

--- push.c
+++ push.c
@@ -2,40 +2,41 @@
 /**
  * push - pushes an element on to the stack
  * @stack: pointer to the top of the stack
- * @value: value to be pushed onto the stack
+ * @line_number: where instructions appear
  * Return: Nothing
 */
 void push(stack_t **stack, int line_number)
 {
-    char *arg = strtok(NULL, " \t\n");
-    if (arg == NULL)
-    {
-        fprintf(stderr, "L%u: usage push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	char *arg = strtok(NULL, " \t\n");
 
-    char *endptr;
-    long int value = strtol(arg, &endptr, 10);
+	if (arg == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	exit(EXIT_FAILURE);
+	}
 
-    // Check for conversion errors
-    if (*endptr != '\0' && !isspace(*endptr))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	char *endptr;
+	long int value = strtol(arg, &endptr, 10);
 
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	if (*endptr != '\0' && !isspace(*endptr))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+		new_node->n = value;
+		new_node->prev = NULL;
+		new_node->next = *stack;
+
+	if (*stack != NULL)
+	(*stack)->prev = new_node;
+	*stack = new_node;
 }
